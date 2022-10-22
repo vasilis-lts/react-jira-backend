5ed77a1f263ee296f44acded2d27586a85e9e584
vasilis_litsas <vasilis.litsas@architechts.gr>
2022-10-22 17:14:47 +0300

    removed babel

--- settings.js
+++ settings.js
@@ -1,2 +1,5 @@
-export const apikey = 'bW9uaWFyb3Mub3JhbmdlQGdtYWlsLmNvbTpDaWt4UDQ3SExEQUZkMHRIWFd2WjQxNzQ=';
-export const password = 'CikxP47HLDAFd0tHXWvZ4174';
+
+module.exports = {
+  apikey: 'bW9uaWFyb3Mub3JhbmdlQGdtYWlsLmNvbTpDaWt4UDQ3SExEQUZkMHRIWFd2WjQxNzQ=',
+  password: 'CikxP47HLDAFd0tHXWvZ4174'
+}
